799438e5de940004048088e19568755be3b8c249
navigateur77 <nj.compaore@gmail.com>
2022-12-15 02:52:23 -0500

    Third coreection after submission

---
+++ Personal_Blog/script.js
@@ -0,0 +1,7 @@
+/* NAVIGATION MOBILE */
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
